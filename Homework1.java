fa5f0db9bfa7be9686e7cdc141a2955156146846
inoc15 <5.stars.at.midnight@gmail.com>
2024-08-23 12:55:16 -1000

    Homework1.java

---
+++ Homework1.java
@@ -0,0 +1,30 @@
+
+public class Practicing_System_and_Primitives {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Task 1: Print out your name using the letter "O" in ASCII art.
+System.out.println("000 000  0  00  000 0   0   000");
+System.out.println(" 0  0   0 0 0 0 0   0   0   0  ");
+System.out.println(" 0  0   0 0 0 0 0   0   0   0  ");
+System.out.println(" 0  000 0 0 00  00  0   0   00 ");
+System.out.println(" 0    0 000 0 0 0   0   0   0  ");
+System.out.println("000 000 0 0 00  000 000 000 000");
+
+
+//Task 2: Write code to compute the following math problem:
+System.out.println(((6.0*3.5)-(1.5*5))/(55.6-30.2));
+
+//Task 3: Write adaptable code that will print out the circumference and area of a circle that has a radius of 4.0
+double pi=(3.1519);
+double cir=(2.0*4.0);
+int are=(4*4);
+System.out.println("The circumference of the circle is ");
+System.out.print(cir*pi);
+System.out.print(" and the area of the circle is ");
+System.out.print(pi*are);
+
+
+	}
+
+}
