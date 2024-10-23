7ed95761c8d2210a4a247974afe182210c48d0e3
inoc15 <5.stars.at.midnight@gmail.com>
2024-10-23 07:56:58 -1000

    HW_StringIntroPractice.java

--- HW_StringIntroPractice.java
+++ HW_StringIntroPractice.java
@@ -16,12 +16,12 @@ public class HW_StringIntroPractice
 		
 		//Prints the statements for questions 2-5
 		printSecondAndLastLetter(myInput);
-		System.out.println(findTheE(myInput));
-		System.out.println(getLength(myInput));
+		System.out.println("The index of the first letter e is "+findTheE(myInput));
+		System.out.println("The amount of characters in this word is "+getLength(myInput));
 		printFirstAndLastThree(myInput);
 		
 		myReader.close();
-		
+
 	}
 	
 	
@@ -30,6 +30,7 @@ public class HW_StringIntroPractice
 	 */
 	public static void printSecondAndLastLetter(String x)
 	{
+		//ystem.out.print(x.length());
 		System.out.print("The second letter of the word is "+x.charAt(1));
 		System.out.println(" and the last letter is "+x.charAt(x.length()-1));
 	}
@@ -38,18 +39,18 @@ public class HW_StringIntroPractice
 	/*
 	 * returns the index of the first 'e' character in a passed String
 	 */
-	public static String findTheE(String e)
+	public static int findTheE(String e)
 	{
-		return("The index of the first letter e is "+e.indexOf("e"));
+		return(e.indexOf("e"));
 	}
 	
 	
 	/*
 	 * returns the amount of characters (the length) of a passed String
 	 */
-	public static String getLength(String l)
+	public static int getLength(String l)
 	{
-		return("The amount of characters in this word is "+l.length());
+		return(l.length());
 	}
 	
 	
@@ -61,8 +62,8 @@ public class HW_StringIntroPractice
 		System.out.print("The first three characters are "+FirstLastThree.substring(0,3));
 		System.out.print(" and the last three characters are "+FirstLastThree.substring(FirstLastThree.length()-3));
 	}
-
-  
+		
 }
+
 
 
