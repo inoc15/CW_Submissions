7ea83b293aa472c71bd7c329fc3d9bd497477b9f
inoc15 <5.stars.at.midnight@gmail.com>
2024-10-30 10:41:41 -1000

    Logic2CodingBat.java

--- CodingBatSolns/Logic2CodingBat.java
+++ CodingBatSolns/Logic2CodingBat.java
@@ -37,6 +37,41 @@ return "x";
 
 
 /*
+*Given a number n, return true if n is in the range 1..10, inclusive. Unless outsideMode is true, in which case return true if the number is less or equal to 1, or greater or equal to 10.
+*/
+public boolean in1To10(int n, boolean outsideMode) 
+{
+  if (outsideMode==false)
+  {
+    if (n>=1 && n<=10)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (outsideMode==true)
+  {
+    if (n<=1||n>=10)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else
+  {
+    return false;
+  }
+}
+
+
+
+/*
 *Returns true if the given non-negative number is a multiple of 3 or 5, but not both. 
 */
 public boolean old35(int n) 
