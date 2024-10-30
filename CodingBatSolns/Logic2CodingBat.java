b9a9b879b31ba4fa90950715bba2be8a57542281
inoc15 <5.stars.at.midnight@gmail.com>
2024-10-30 10:35:32 -1000

    Logic2CodingBat.java

--- CodingBatSolns/Logic2CodingBat.java
+++ CodingBatSolns/Logic2CodingBat.java
@@ -1,3 +1,6 @@
+/*
+*Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off"
+*/
 public String alarmClock(int day, boolean vacation) 
 {
   if (vacation==true)
@@ -30,3 +33,25 @@ public String alarmClock(int day, boolean vacation)
 return "x";
   
 }
+
+
+
+/*
+*Returns true if the given non-negative number is a multiple of 3 or 5, but not both. 
+*/
+public boolean old35(int n) 
+{
+  if (n%3==0&&n%5==0)
+  {
+    return false;
+  }
+  else if (n%3==0||n%5==0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
