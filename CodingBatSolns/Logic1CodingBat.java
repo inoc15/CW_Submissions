a97711b6650378784f73eebb17b8a0ee04e76079
inoc15 <5.stars.at.midnight@gmail.com>
2024-11-06 08:54:43 -1000

    Logic1CodingBat.java

--- CodingBatSolns/Logic1CodingBat.java
+++ CodingBatSolns/Logic1CodingBat.java
@@ -125,3 +125,20 @@ public int caughtSpeeding(int speed, boolean isBirthday)
   }
 }
 
+
+
+/*
+* sortaSum: Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+*/
+public int sortaSum(int a, int b) 
+{
+  if (a+b>=10 && a+b<=19)
+  {
+    return 20;
+  }
+  else
+  {
+    return a+b;
+  }
+}
+
