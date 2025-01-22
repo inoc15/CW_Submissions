2e6afab4f778fac424ac849333c90cb6b11aa792
inoc15 <5.stars.at.midnight@gmail.com>
2025-01-22 10:06:17 -1000

    ArraysCodingBat2.java

--- CodingBatSolns/ArraysCodingBat2.java
+++ CodingBatSolns/ArraysCodingBat2.java
@@ -26,8 +26,28 @@ public int[] reverse3(int[] nums)
 /*
 * maxEnd3
 */
-
-
+public int[] maxEnd3(int[] nums) 
+{
+  if (nums[0]==nums[2])
+  {
+    nums[1] = nums[2];
+  }
+  else if (nums[2]>nums[0])
+  {
+    nums[0]=nums[2];
+    nums[1]=nums[2];
+  }
+  else if (nums[0]>nums[2])
+  {
+    nums[1]=nums[0];
+    nums[2]=nums[0];
+  }
+  else
+  {
+    return nums;
+  }
+  return nums;
+}
 
 
 /*
