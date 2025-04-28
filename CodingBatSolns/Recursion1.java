cb4e328d5ea9b1b1422f2e5eb6c38a3394aebed1
inoc15 <5.stars.at.midnight@gmail.com>
2025-04-28 12:30:50 -1000

    Recursion1.java

--- CodingBatSolns/Recursion1.java
+++ CodingBatSolns/Recursion1.java
@@ -1,5 +1,6 @@
 /*
 * Coding Bat recursion 1
+* Authored by: isabelle Inocencio
 */
 
 //recursion 1: factorial 
@@ -28,4 +29,21 @@ public int bunnyEars(int bunnies)
   }
 }
 
+
+//recursion 1: Fibonacci Sequence 
+public int fibonacci(int n) 
+{
+  if (n==1)
+  {
+    return 1;
+  }
+  if ( n==0)
+  {
+    return 0;
+  }
+  else
+  {
+    return fibonacci(n-2)+fibonacci(n-1);
+  }
+}
 
